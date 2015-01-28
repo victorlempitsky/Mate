3ffc01b9778198a683748fd0139ee75abe1e7e59
victorlempitsky <victorlempitsky@gmail.com>
2015-01-28 11:06:10 +0300

    Several revisions of examples and softmaxloss layer

---
+++ examples/MateMnistAllDist.m
@@ -0,0 +1,91 @@
+function MateMnistAllDist
+%demonstrates Mate on MNIST
+%example is derived from the analogous MatConvNet example
+
+[thispath,~,~] = fileparts(mfilename('fullpath'));
+dataDir = [thispath '/data/mnist'] ;
+expDir = [thispath '/data/exp'] ;
+useGpu = true;
+
+dataset = struct;
+dataset.imdb = getMnistImdb(dataDir) ;
+
+% Define the network
+
+f=1/100 ;
+net = MateNet( {
+  MateConvLayer(f*randn(5,5,1,20, 'single'), zeros(1, 20, 'single'), ...
+                'stride', 1, 'pad', 0, 'name', 'conv1')
+  MatePoolLayer('pool',[2 2], 'stride', 2, 'pad', 0)
+  MateConvLayer(f*randn(5,5,20,50, 'single'), zeros(1, 50, 'single'), ...
+                'stride', 1, 'pad', 0, 'weightDecay', [0.005 0.005])
+  MatePoolLayer('pool',[2 2], 'stride', 2, 'pad', 0)  
+  MateConvLayer(f*randn(4,4,50,500, 'single'), zeros(1, 500, 'single'), ...
+                'stride', 1, 'pad', 0, 'weightDecay', [0.005 0.005])  
+  MateReluLayer
+  MateConvLayer(f*randn(1,1,500,10, 'single'), zeros(1, 10, 'single'), ...
+                'stride', 1, 'pad', 0, ...
+                'weightDecay', [0.005 0.005])
+  MateSqueezeLayer
+  MateAllDistLayer('name','distances')  
+  MateMetricHingeLossLayer('takes',{'distances','input:2'},...
+        'name','loss', 'PositiveWeight', 10)
+  MateRankErrorLayer('takes',{'dist','input:3'},'invert',true,'name','rankError')
+  } );
+
+
+%subtract mean
+dataset.imdb.images.data = bsxfun(@minus, dataset.imdb.images.data,...
+            mean(dataset.imdb.images.data,4)) ;
+          
+%move to GPU         
+if useGpu
+  net = net.move('gpu');
+  dataset.imdb.images.data = gpuArray(dataset.imdb.images.data) ;
+  dataset.imdb.images.labels = gpuArray(dataset.imdb.images.labels) ;
+end
+
+dataset.train = find(dataset.imdb.images.set == 1);
+dataset.val = find(dataset.imdb.images.set == 3);
+dataset.batchSize = 100;
+dataset.train = dataset.train(randperm(numel(dataset.train)));
+dataset.val = dataset.val(randperm(numel(dataset.val)));
+
+[net,info,dataset] = net.trainNet(@getBatch, dataset,...
+     'numEpochs',100, 'continue', true, 'expDir', expDir,...
+     'learningRate', 0.001,'monitor', {'loss','rankError'},...
+     'showLayers', {'conv1','distances'},'onEpochEnd', onEpochEnd) ;
+
+%----------------------------------------------------------%
+
+function [x, eoe, dataset] = getBatch(istrain, batchNo, dataset)
+eoe=false;
+batchStart = batchNo*dataset.batchSize+1;
+batchEnd = (batchNo+1)*dataset.batchSize;
+
+if istrain
+  if batchEnd >= numel(dataset.train)
+    batchEnd = numel(dataset.train);
+    eoe = true; %end of epoch
+  end
+  batch = dataset.train(batchStart:batchEnd);
+  labels = dataset.imdb.images.labels(1,batch) ;
+else
+  if batchEnd >= numel(dataset.val)
+    batchEnd = numel(dataset.val);
+    eoe = true; %end of epoch
+  end
+  batch = dataset.val(batchStart:batchEnd); 
+  labels = dataset.imdb.images.labels(1,batch) ;
+  [labels,order] = sort(labels);
+  batch = batch(order);
+end
+
+x{1} = dataset.imdb.images.data(:,:,:,batch) ;
+x{2} = single(bsxfun(@eq,labels.labels'));
+
+
+%--------------------------------------------------------------%
+function [net,dataset] = onEpochEnd(net,dataset)
+dataset.train = dataset.train(randperm(numel(dataset.train)));
+
---
+++ toolbox/MateAllDistLayer.m
@@ -0,0 +1,20 @@
+classdef MateAllDistLayer < MateLayer
+  methods
+    function obj = MateAllDistLayer(varargin)
+      obj@MateLayer(varargin);
+    end
+    
+    function [y,obj] = forward(obj,x)
+      sz = size(x);
+      x_ = reshape(x, prod(sz(1:end-1)), sz(end));
+      len = sum(x_.*x_);
+      y = bsxfun(@plus, len, len').*single(0.5) - x_'*x_;
+    end
+    
+    function [dzdx,obj] = backward(obj, x, dzdy, y)
+      sz = size(x);
+      x_ = reshape(x, prod(sz(1:end-1)), sz(end))';      
+      dzdx = reshape(bsxfun(@times, x_, sum(dzdy,1))-x_*dzdy, sz);
+    end
+  end  
+end
