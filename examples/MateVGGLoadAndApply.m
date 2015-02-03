406cdd92c8734c906ede8533bfe9b830bb2bc948
victorlempitsky <victorlempitsky@gmail.com>
2015-02-03 17:54:27 +0300

    Loading MatConvNet models

--- examples/MateVGGLoadAndApply.m
+++ examples/MateVGGLoadAndApply.m
@@ -1,45 +1,52 @@
 pathto = '/media/storage/vilem/Downloads/';
-fname = 'imagenet-caffe-ref.mat';
+fname = 'imagenet-caffe-alex.mat';
 webloc = 'http://www.vlfeat.org/matconvnet/models/';
 
 if ~exist([pathto fname],'file')
+  disp('Loading model from the VGG website..,');
   websave([pathto fname], [webloc fname]);
+  disp('Done...');
 end
 
 [layers,classes] = readMatConvNet([pathto fname]);
 
 
 %trying on peppers
+
+%using MatConvNet model + an argmax layer
 net = MateNet( cat(1,layers, {MateArgmaxLayer('name','class')}) );
 
 im = imread('peppers.png');
 net = net.makePass(im);
 
 imshow(im);
-fprintf('This image is classified as "%s".\n', classes.description{net.getBlob('class')});
+fprintf('This image is classified as "%s".\n',...
+          classes.description{net.getBlob('class')});
 
 %testing speed in batch mode
-net = MateNet(layers(2:end)); %same network without preprocessor and class selector
+net = MateNet(layers(2:end)); %same network without preprocessor and argmax
 batchSz = 256;
-im = layers{1}.forward(im);
-batch = repmat(im,[1 1 1 batchSz]);
+im = layers{1}.forward(im); %preprocess an image
+batch = repmat(im,[1 1 1 batchSz]); 
 
-net.makePass(batch);
+net = net.makePass(batch); %warm-up
 %now measuring
 tic
-net.makePass(batch);
+net = net.makePass(batch);
 tm = toc;
-fprintf('\n Batch forward-passed on CPU in %f sec', tm);
+fprintf('Batch forward-passed on CPU in %f sec.\n', tm);
 
 
 %let us measure on GPU
 batch = gpuArray(batch);
 net = net.move('gpu');
-%warm-up run
-net.makePass(batch);
+
+net = net.makePass(batch);%warm-up
 %now measuring
 tic
-net.makePass(batch);
+for i=1:10
+  net = net.makePass(batch);
+end
 tm = toc;
-fprintf('\n Batch forward-passed on GPU in %f sec', tm);
+fprintf('Batch forward-passed on GPU in %f sec.\n', tm/10);
 
